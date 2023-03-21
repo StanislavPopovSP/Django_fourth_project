ae9a8b637de0f2a1ec824faa6e4e401d415df285
StanislavPopovSP <staspopov4@gmail.com>
2023-03-21 21:33:19 +0500

    Добавил путь для просмотра каждой отдельной карточки, обработчик кот-й будет загружать на html шаблон информацию об этой карточке.

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -5,3 +5,9 @@ def projects(request):
     pr = Project.objects.all()
     context = {'projects': pr}
     return render(request, 'projects/projects.html', context)
+
+
+def project(request, pk):
+    """Функция, будет возвращать страницу просмотра отдельного проекта"""
+    project_obj = Project.objects.get(id=pk)
+    return render(request, 'projects/single-project.html', {'project': project_obj})
