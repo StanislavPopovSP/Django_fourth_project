f4fd021261556af6b2e58fca95ec0f22fb312d5b
StanislavPopovSP <staspopov4@gmail.com>
2023-03-22 20:03:32 +0500

    Добавил 1 проект(карточку), обновил стили в форме через класс ProjectForm, наследуя ранее прописанные стили.

--- devsearch/projects/forms.py
+++ devsearch/projects/forms.py
@@ -5,3 +5,16 @@ class ProjectForm(ModelForm):
     class Meta: # Что бы конкретно что-то выводилось на нашу страницу, какие поля попадали в элементы формы, нужен класс Meta.
         model = Project
         fields = ['title', 'featured_image', 'description', 'demo_link', 'source_link', 'tags'] # какие поля попадут в форму
+
+        # Добавим к форме стили которые предустановлены.
+    def __init__(self, *args, **kwargs):
+        """Инициализатор будет использовать любые элементы из родительского элемента, через наследование родительского класса."""
+        super().__init__(*args, **kwargs) # наследуемся от ModelForm
+        # .widget.attrs.update - виджеты, атрибуты, обновить(в виде словаря передать любые атрибуты которые мы хотим)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
+
+        # Что бы каждое поле не прописывать сделаем цикл for
+        # self.fields['title'].widget.attrs.update({'class': 'input'})  # Что бы внешний вид формы поменялся, нужно иметь возможность из род-го класса испоьзовать какие-то виджеты и их атрибуты.
+        # self.fields['description'].widget.attrs.update({'class': 'input'})  # Что бы внешний вид формы поменялся, нужно иметь возможность из род-го класса испоьзовать какие-то виджеты и их атрибуты.
