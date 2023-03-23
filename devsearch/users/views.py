4ed246f055927b6cf005fca6748ff3b13d615aa5
StanislavPopovSP <staspopov4@gmail.com>
2023-03-23 19:43:56 +0500

    Добавил переход на профиль, путь к профелю, обработчик.

--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from .models import Profile
 
+
 def profiles(request):
     prof = Profile.objects.all()
     context = {'profiles': prof}
     return render(request, 'users/index.html', context)
+
+
+def user_profile(request, pk):
+    return render(request, 'users/profile.html')
