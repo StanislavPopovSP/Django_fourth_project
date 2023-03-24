3ded26276fa15a403ba30a22e14f38271fbb0f4b
StanislavPopovSP <staspopov4@gmail.com>
2023-03-24 12:49:31 +0500

    Дописал обработчик, шаблон профиля

--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -10,7 +10,13 @@ def profiles(request):
 
 def user_profile(request, pk):
     prof = Profile.objects.get(id=pk)
+
+    top_skill = prof.skill_set.exclude(description__exact="") # Исключаю элементы у которых описание пустое. description__exact="" - исключить элементы, которые будут содержать пустую строку(в виде описания).
+    other_skills = prof.skill_set.filter(description="") # Будем выводить элементы с пустым описанием
+
     context = {
-        'profile': prof
+        'profile': prof,
+        'top_skills': top_skill,
+        'other_skills': other_skills,
     }
     return render(request, 'users/profile.html', context)
