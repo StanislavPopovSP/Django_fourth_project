bfecd16b82b5d11c0e76ab5cc512d9785f4d6757
StanislavPopovSP <staspopov4@gmail.com>
2023-03-25 22:05:30 +0500

    Прикрепил стили в форму

--- devsearch/users/forms.py
+++ devsearch/users/forms.py
@@ -1,11 +1,21 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+
 class CustomUserCreationForm(UserCreationForm):
     """Класс, отвечает за создание своей формы, для регистрации пользователя"""
+
     class Meta:
         model = User
         fields = ['first_name', 'email', 'username', 'password1', 'password2']
         labels = {
             'first_name': 'Name'
         }
+
+    def __init__(self, *args, **kwargs):
+        """Инициализатор будет использовать любые элементы из родительского элемента, через наследование родительского класса."""
+        super().__init__(*args, **kwargs)  # наследуемся от ModelForm
+        # .widget.attrs.update - виджеты, атрибуты, обновить(в виде словаря передать любые атрибуты которые мы хотим)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
