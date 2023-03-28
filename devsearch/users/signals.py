07bdf64a5c12059e28826432a51f6ad868b677a2
StanislavPopovSP <staspopov4@gmail.com>
2023-03-28 18:12:13 +0500

    Добавил функцию отвечающую за редактирование пользователя на странице account.html

--- devsearch/users/signals.py
+++ devsearch/users/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save  # Сигналы уведомления в админке. Нам надо что бы когда создавался пользователь, что бы профиль создавался автоматически.
+from django.db.models.signals import post_save  # post_save - сохранение данных. Сигналы уведомления в админке. Нам надо что бы когда создавался пользователь, что бы профиль создавался автоматически.
 from django.db.models.signals import post_delete
 from django.dispatch import receiver  # декоратор для подключения
 from .models import Profile
@@ -9,9 +9,9 @@ from django.contrib.auth.models import User
 # instance - как экземпляр текущий
 # created - созданный, если профиль был создан.
 # **kwargs - набор именованных параметров.
-# @receiver(post_save, sender=User)
+@receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
-    """Функция, объединяет при создании в админке пользователя, автоматически создается профиль пользователя."""
+    """Функция, объединяет пользователя с его профилем, при создании."""
     print('Profile signal!')
     if created:
         user = instance
@@ -22,13 +22,27 @@ def create_profile(sender, instance, created, **kwargs):
             name=user.first_name
         )
 
+@receiver(post_save, sender=Profile)
+def update_user(sender, instance, created, **kwargs):
+    """Функция, отвечает за редактирование пользователя"""
+    profile = instance # это какой конкретный профиль пользователя будет обновляться.
+    user = profile.user # в user будет профиль пользователя сохраняться
 
-# @receiver(post_delete, sender=Profile)
+    if created is False: # если пользователя не создаем, тогда это идёт обновление.
+        user.first_name = profile.name # Что бы данные перезаписывались, относительно тех которые существуют.
+        user.username = profile.username
+        user.email = profile.email
+        user.save() # сохраняем в БД
+
+#
+@receiver(post_delete, sender=Profile)
 def delete_user(sender, instance, **kwargs):
+    """Функция, отвечает за удаление пользователя"""
     print('Deleting user...')
     user = instance.user
     user.delete()
-
-# Можем сделать не таким способом а через дикаратор
-post_save.connect(create_profile, sender=User) # первым параметром передаем имя функции без вызова
-post_delete.connect(delete_user, sender=Profile)
+#
+# # Можем сделать не таким способом а через дикаратор
+# post_save.connect(create_profile, sender=User) # первым параметром передаем имя функции без вызова.
+# post_save.connect(update_user, sender=Profile) # запускаем, обновлять будем профиль пользователя, а не самого пользователя.
+# post_delete.connect(delete_user, sender=Profile)
