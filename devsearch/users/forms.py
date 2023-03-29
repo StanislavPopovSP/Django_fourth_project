fbb0ba0824a9359028815a39f451f71ef5caea2d
StanislavPopovSP <staspopov4@gmail.com>
2023-03-29 20:00:23 +0500

    Добавил путь в account.html для перехода на форму навыков(skills), обработчик create_skill. Создал саму форму SkillForm

--- devsearch/users/forms.py
+++ devsearch/users/forms.py
@@ -1,7 +1,22 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Profile
+from .models import Profile, Skill
 from django.forms import ModelForm # что бы данные брались из нашей модели
+
+
+class SkillForm(ModelForm):
+    class Meta:
+        model = Skill
+        fields = '__all__' # Можем взять из модели все поля
+        exclude = ['owner', 'created'] # Исключаем поля которые нам не нужны
+
+    def __init__(self, *args, **kwargs):
+        """Инициализатор будет использовать любые элементы из родительского элемента, через наследование родительского класса."""
+        super().__init__(*args, **kwargs)  # наследуемся от ModelForm
+        # .widget.attrs.update - виджеты, атрибуты, обновить(в виде словаря передать любые атрибуты которые мы хотим)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
 
 
 class ProfileForm(ModelForm):
