425201a505eeb9b5055b4ef29a2d7662e435b901
StanislavPopovSP <staspopov4@gmail.com>
2023-03-30 18:59:52 +0500

    Добавил в обработчик projects пагинацию, начал заполнять projects.html пагинацию.

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -2,12 +2,29 @@ from django.shortcuts import render, redirect
 from .models import Project, Tag
 from .forms import ProjectForm
 from django.contrib.auth.decorators import login_required  # Декоратор для того что бы только пользователь смог что то делать
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage  # Для работы с пагинацией страниц PageNotAnInteger - работа с ошибками EmptyPage - пустая страница.
 
 
 def projects(request):
-    """Функция, отвечает за доступ ко всем объектам таблицы"""
+    """Доступ ко всем объектам таблицы, пагинация"""
     pr = Project.objects.all()
-    context = {'projects': pr}
+    page = request.GET.get('page') # нужно создать страницу (получаем данные методом get саму страницу)
+    results = 3 # сколько результирующих элементов будет на странице
+    paginator = Paginator(pr, results) # кол-во элементов из всех данных которые мы берем.
+
+    try:
+        pr = paginator.page(page) # получаем саму страницу (если страниц несколько, то будем присваивать все страницы)
+    except PageNotAnInteger: # если страниц не найдено
+        page = 1 # если других страниц не будет, страница должна быть только одна. (если больше числовых значений не найдено, то присваиваем 1 )
+        pr = paginator.page(page)
+    except EmptyPage: # если страница пустая
+        page = paginator.num_pages  # .num_pages - что бы кол-во страниц приходило(если 0, то значит 0).
+        pr = paginator.page(page)
+
+    context = {
+        'projects': pr,
+        'paginator': paginator
+    }
     return render(request, 'projects/projects.html', context)
 
 
